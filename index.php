3551d759f45752d3b4dd33d39257861cc4ef2519
Diego Chan <diego_chan09@hotmail.com>
2016-06-27 10:00:08 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+</head>
+<body>
+	<h2>Página principal</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero eos debitis natus magnam vel id repudiandae voluptate explicabo, officia quasi excepturi sapiente esse omnis ea tempora voluptatum architecto, eius voluptatem.</p>
+
+	<h2>Encabezado h2</h2>
+</body>
+</html>
